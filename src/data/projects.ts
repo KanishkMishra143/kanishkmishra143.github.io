7bd072f166cc72e15f18a1ac96aab006da952fe3
KanishkMishra143 <kanishk.mishra012@adgitmdelhi.ac.in>
2026-03-22 22:32:37 +0530

    Added ISC and reordered projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,13 +7,6 @@ export interface Project {
 
 export const projectsData: Project[] = [
     {
-        name: "NextStep – Career Recommendation Platform",
-        description: "Backend development for a career advisory system (SIH 2025). Implemented APIs to process psychological assessment parameters and map results to career paths.",
-        tags: ['Node.js', 'Express', 'API Design', 'Backend'],
-        // Check if this URL is correct or add the real one
-        url: "https://github.com/KanishkMishra143/NextStep", 
-    },
-    {
         name: "NotionSafe – Secure Notion Backup Tool",
         description: "A CLI with cross-platform GUI to securely back up Notion workspace data via official APIs. Designed for automation, data integrity, and fault tolerance.",
         tags: ['Python', 'CLI', 'Notion API', 'Automation'],
@@ -25,6 +18,20 @@ export const projectsData: Project[] = [
         description: "Linux: Command-line utility to switch PipeWire/PulseAudio sinks. Windows: Native Go application that interacts directly with Windows Core Audio APIs.",
         tags: ['Windows', 'Linux', 'Python', 'GO','CLI', 'PipeWire', 'Fedora COPR', 'Arch User Repository(AUR)'],
         url: "https://github.com/KanishkMishra143/sink-switch",
+    },
+    {
+        name: "NextStep – Career Recommendation Platform",
+        description: "Backend development for a career advisory system (SIH 2025). Implemented APIs to process psychological assessment parameters and map results to career paths.",
+        tags: ['Node.js', 'Express', 'API Design', 'Backend'],
+        // Check if this URL is correct or add the real one
+        url: "https://github.com/KanishkMishra143/NextStep", 
+    },
+    {
+        name: "Interactive Symlink Creator – File Management Tool",
+        description: "Interactive Bash utility to create and manage symbolic links with user-friendly prompts.Designed for safe file operations and streamlined workflow automation in Linux environments.",
+        tags: ['Bash', 'Linux'],
+        // Check if this URL is correct
+        url: "https://github.com/KanishkMishra143/Interactive-Symlink-Creator", 
     },
     {
         name: "Alien Invasion – Python Game",
